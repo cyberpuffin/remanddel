0e5abf5572bc2d6c3d8b7afb0e30dcd0479a79ec
Andrew Nase <drew.nase@drew.nase-C02P61W5G3QT>
2016-11-09 13:58:37 -0500

    added enableProdMode

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
+import {enableProdMode} from '@angular/core';
 
 bootstrap(AppComponent);
+enableProdMode();
